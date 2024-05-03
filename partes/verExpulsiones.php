46f311517e6a466e1b100432ff4bf5fb2ba7adb2
Sergioseves <92640821+Sergioseves@users.noreply.github.com>
2024-05-03 11:14:01 +0200

    Update verExpulsiones.php

--- partes/verExpulsiones.php
+++ partes/verExpulsiones.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Partes de la Base de Datos</title>
+    <title>Expulsiones Pendientes</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
